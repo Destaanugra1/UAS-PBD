5c92dce3625e50e9ea0c69b23fac017c8ea54b07
iky <154492576+ikykurniawan-2410@users.noreply.github.com>
2025-02-05 09:40:47 +0700

    memeprbaiki hapus penelitiann

--- hapus/hapus_penelitian.php
+++ hapus/hapus_penelitian.php
@@ -2,7 +2,17 @@
 include "../koneksi.php";
 
 $id_penelitian_1 = $_GET['id_penelitian_1'];
+
+// penghapusan record pertama
+mysqli_query($mysqli, "DELETE FROM anggota_penelitian_1 WHERE id_penelitian_1 ='$id_penelitian_1'");
+mysqli_query($mysqli, "DELETE FROM pendanaan_eksternal_1 WHERE id_penelitian_1 ='$id_penelitian_1'");
+mysqli_query($mysqli, "DELETE FROM luaran_penelitian_1 WHERE id_penelitian_1 ='$id_penelitian_1'");
+mysqli_query($mysqli, "DELETE FROM konferensi_1 WHERE id_penelitian_1 ='$id_penelitian_1'");
+mysqli_query($mysqli, "DELETE FROM hki_1 WHERE id_penelitian_1 ='$id_penelitian_1'");
+
+// penghapusan utama berdsarkan fk table penelitian_1
 mysqli_query($mysqli, "DELETE FROM penelitian_1 WHERE id_penelitian_1 ='$id_penelitian_1'");
 
+// Redirect back
 header("location: ../database/penelitian.php");
 ?>
